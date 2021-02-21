47cc12db4bdafee52fd542e18884ff994cf465ab
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-20 21:15:13 -0300

    Adicionando a resolucao a exprecao lambda

--- Comparison-Treino1/Program.cs
+++ Comparison-Treino1/Program.cs
@@ -13,7 +13,7 @@ namespace Comparison_Treino1 {
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProducts;
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
             list.Sort(comp);
 
@@ -23,9 +23,6 @@ namespace Comparison_Treino1 {
 
         }
 
-        static int CompareProducts(Product p1, Product p2) {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-        }
 
     }
 }
